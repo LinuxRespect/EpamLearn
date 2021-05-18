99744c80c14131fb73cf93dce6f5bb245ec8b3c9
Oleh <respect305305@i.ua>
2021-05-18 14:21:31 +0300

    main task part Java Classes

--- src/main/java/javaClasses/MainTask.java
+++ src/main/java/javaClasses/MainTask.java
@@ -10,33 +10,36 @@ public class MainTask {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMMM d, yyyy", Locale.ENGLISH);
         Student students[] = new Student[5];
-        students[0] = new Student(1, "Petrov", "Petr", "Ivanovich",formatter.parse("Sat, April 4, 2000"),
-                "Kiev", "52341345", "rpd", "1", "5");
-        students[1] = new Student(2, "Petrov2", "Petr", "Ivanovich",formatter.parse("Sat, April 4, 2001"),
-                "Kiev", "52341345", "fifo", "2", "5");
-        students[2] = new Student(3, "Petrov3", "Petr", "Ivanovich", formatter.parse("Sat, April 4, 2002"),
-                "Kiev", "52341345", "mhm", "2", "3");
-        students[3] = new Student(4, "Petrov4", "Petr", "Ivanovich", formatter.parse("Sat, April 4, 2003"),
-                "Kiev", "52341345", "rpd", "1", "5");
-        students[4] = new Student(5, "Petrov5", "Petr", "Ivanovich", formatter.parse("Sat, April 4, 2002"),
-                "Kiev", "52341345", "rpd", "1", "4");
+        students[0] = new Student(1, "Petrov", "Petr", "Ivanovich",
+                formatter.parse("Sat, April 4, 2000"), "Kiev", "52341345",
+                "rpd", "1", "5");
+        students[1] = new Student(2, "Petrov2", "Petr", "Ivanovich",
+                formatter.parse("Sat, April 4, 2001"), "Kiev", "52341345",
+                "fifo", "2", "5");
+        students[2] = new Student(3, "Petrov3", "Petr", "Ivanovich",
+                formatter.parse("Sat, April 4, 2002"), "Kiev", "52341345",
+                "mhm", "2", "3");
+        students[3] = new Student(4, "Petrov4", "Petr", "Ivanovich",
+                formatter.parse("Sat, April 4, 2003"), "Kiev", "52341345",
+                "rpd", "1", "5");
+        students[4] = new Student(5, "Petrov5", "Petr", "Ivanovich",
+                formatter.parse("Sat, April 4, 2002"), "Kiev", "52341345",
+                "rpd", "1", "4");
 
-        //список студентов заданного факультета;
-        List<Student> studentRPDfaculty =Arrays.stream(students)
+        //a) список студентов заданного факультета;
+        List<Student> studentRPDfaculty = Arrays.stream(students)
                 .filter(student -> student.department.equals("rpd"))
                 .collect(Collectors.toList());
-        System.out.println("List student where faculty equal rpd :"+ studentRPDfaculty.toString());
+        System.out.println("List student where faculty equal rpd :" + studentRPDfaculty.toString());
 
-      /*  //списки студентов для каждого факультета и курса;
-        Arrays.stream(students)
-                .filter(student -> {
-                    student.department.
-                })*/
+        //b)списки студентов для каждого факультета и курса;
+        List<Student> definetlyStudentRPDfacultyFirstCourse = Arrays.stream(students)
+                .filter(student -> student.department.equals("rpd") && student.course.equals("1"))
+                .collect(Collectors.toList());
+        System.out.println("List student where faculty equal rpd and course=1 :" +
+                definetlyStudentRPDfacultyFirstCourse.toString());
 
-
-        //список студентов, родившихся после заданного года;
-
-
+        //c) список студентов, родившихся после заданного года;
         Date dateFrom = null;
         try {
             dateFrom = formatter.parse("Sat, April 4, 2002");
@@ -45,18 +48,16 @@ public class MainTask {
         }
 
         Date finalDateFrom = dateFrom;
-        List<Student> studentAfter2001 =Arrays.stream(students)
-                .filter(student -> student.getBirthDay().getTime()> finalDateFrom.getTime())
+        List<Student> studentAfter2001 = Arrays.stream(students)
+                .filter(student -> student.getBirthDay().getTime() > finalDateFrom.getTime())
                 .collect(Collectors.toList());
-        System.out.println("List students which born after Sat, April 4, 2002  :"+ studentAfter2001.toString());
+        System.out.println("List students which born after Sat, April 4, 2002  :" + studentAfter2001.toString());
 
         //d) список учебной группы.
-
-
         List<Student> studentListGroup = Arrays.stream(students)
                 .filter(student -> student.getGroup().equals("3"))
                 .collect(Collectors.toList());
-        System.out.println("List students where group equal=3 :"+ studentListGroup.toString());
+        System.out.println("List students where group equal=3 :" + studentListGroup.toString());
 
     }
 }
